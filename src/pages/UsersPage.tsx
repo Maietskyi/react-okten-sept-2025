bf6c8f4410ab6926b78e704f36ba6e11fe10f2e1
Maietskyi <damaietskyi@gmail.com>
2025-01-15 01:10:09 +0200

    Add Home Work 3 task 3

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,10 +1,8 @@
 import {UsersComponent} from "../components/users-component/UsersComponent.tsx";
-import {Outlet} from "react-router-dom";
 
 export const UsersPage = () => {
     return (
         <div>
-            <Outlet/>
             <hr/>
             <UsersComponent/>
         </div>
