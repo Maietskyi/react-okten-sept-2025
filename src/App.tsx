59ae8f2ceed62d09f9aa32ae0f50edcff02b5366
Maietskyi <damaietskyi@gmail.com>
2025-01-17 23:36:15 +0200

    Add Home Work 4 / task_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import {Outlet} from "react-router-dom";
 
-function App() {
+export const App = ()=> {
 
   return (
-    <></>
+      <>
+        <Outlet/>
+      </>
   )
 }
-
-export default App
