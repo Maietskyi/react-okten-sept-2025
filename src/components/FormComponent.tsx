91dbe7d7f3ad6a4ff48e695245ff15c690f3515e
Maietskyi <damaietskyi@gmail.com>
2025-01-18 20:29:52 +0200

    Add Lesson 5 / 5 useForm joi

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,4 +1,6 @@
 import {useForm} from "react-hook-form";
+import userValidator from "../validators/user.validator.tsx";
+import {joiResolver} from "@hookform/resolvers/joi";
 
 interface IFormProps {
     username: string;
@@ -10,54 +12,29 @@ export const FormComponent = () => {
     const {
         handleSubmit,
         register,
-        formState: { errors, isValid }
+        formState: {errors, isValid}
     } = useForm<IFormProps>({
         mode: 'all',
+        resolver:joiResolver(userValidator)
     });
 
-    console.log(register);
     const customeHandler = (formDataProps: IFormProps) => {
         console.log(formDataProps);
     };
     return (
         <div>
             <form onSubmit={handleSubmit(customeHandler)}>
-                <label><input className='border-2' type="text" {...register('username', {
-                    required: true,
-                    // pattern:{
-                    //    value:/\w+/,
-                    //     message:'wrong name',
-                    // }
-                    minLength: {value: 4, message: 'wrong name'}
-                })}/>
-                    {
-                        errors.username && <div>
-                            {errors.username.message}
-                        </div>
-                    }
+                <label>
+                    <input className='border-2' type="text" {...register('username')}/>
+                    {errors.username && <div>{errors.username.message}</div>}
                 </label>
-                <label><input className='border-2' type="text" {...register('password', {
-                    required: true,
-                    minLength: {value: 3, message: 'pass too short'},
-                    maxLength: {value: 6, message: 'pass too long'}
-                })}/>
-                    {
-                        errors.password && <div>
-                            {errors.password.message}
-                        </div>
-                    }
+                <label>
+                    <input className='border-2' type="text" {...register('password')}/>
+                    {errors.password && <div>{errors.password.message}</div>}
                 </label>
-                <label><input className='border-2' type="number" {...register('age', {
-                    required: true,
-                    valueAsNumber: true,
-                    min: {value: 1, message: 'age to small'},
-                    max: {value: 117, message: 'age to big'}
-                })}/>
-                    {
-                        errors.age && <div>
-                            {errors.age.message}
-                        </div>
-                    }
+                <label>
+                    <input className='border-2' type="number" {...register('age')}/>
+                    {errors.age && <div>{errors.age.message}</div>}
                 </label>
                 <button className='border-2' disabled={!isValid}>send</button>
             </form>
