1d4ab9e81b93a8e626a9d6e1458178e0586c39e1
Maietskyi <damaietskyi@gmail.com>
2025-01-19 13:36:04 +0200

    Add Home Work 5 / task_1_ myself

--- src/components/validtors/CarValidator.tsx
+++
@@ -1,8 +0,0 @@
-import * as Joi from "joi";
-
-export const carValidator =Joi.object({
-    brand: Joi.string().pattern(/^[A-Za-z0-9]+$/)
-        .messages({'string.pattern.base':'Your brand value didnt match pattern'}),
-    price: Joi.number().min,
-    year: Joi.number(),
-})
