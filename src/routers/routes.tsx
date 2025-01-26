e6ed81145488c3a11be32061b3ef36964597a7fd
Maietskyi <damaietskyi@gmail.com>
2025-01-26 20:35:55 +0200

    Add Home Work 6

--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -3,12 +3,18 @@ import MainLayout from "../layouts/MainLayout.tsx";
 import HomePage from "../pages/HomePage.tsx";
 import LoginPage from "../pages/LoginPage.tsx";
 import AuthResourcesPage from "../pages/AuthResourcesPage.tsx";
-
+// імпорти потрібних компонентів і сторінок
 
 export const routes = createBrowserRouter([
+    // експортуємо оголошену змінну routes для переходу на сторінки, змінна містить маршрути програми
     {
+        // оголошую загальний path: '/', що є моєю головною (домашньою) сторінкою
+        // element: <MainLayout/> — базова сторінка, яка і відображається для маршруту '/'
         path: '/', element: <MainLayout/>, children:[
+            // Додаю children, тобто дочірні маршрути для головної сторінки
+            // на HomePage і буде відображатись вся інформація для головної сторінки
             {index:true, element:<HomePage/>},
+            // path: 'login' відповідає шляху '/login'
             {path:'login',element:<LoginPage/>},
             {path:'/auth/resources',element:<AuthResourcesPage/>}
         ]
