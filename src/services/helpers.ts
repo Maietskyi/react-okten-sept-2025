c043519e8184cfe55bc79296f97f64b4ac3f5bff
Maietskyi <damaietskyi@gmail.com>
2025-01-27 01:42:22 +0200

    Add Home Work 6

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -1,8 +1,14 @@
+// експортую метод в якому є дженерік, і визначаю по якому ключу я хочу шукати
 export const retriveLocalStorage = <T> (key:string) => {
+    // в змінній object дістаю з localStorage значення ключа
     const object = localStorage.getItem(key);
+    // у випадку якщо object не існує
     if (!object) {
+        // то повертаю пустий об'єкт строго типізований так як початковий тип даних
         return {} as T;
     }
+    //  у випадку якщо object існує, я роблю змінну parse яка буде парсити object
     const parse = JSON.parse(object);
+    // і повертаю парс строго типізований
     return parse as T;
 }
