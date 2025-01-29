07c98f9424087dcfd0cb709707439d324a0db86e
Maietskyi <damaietskyi@gmail.com>
2025-01-30 00:33:15 +0200

    Add Home Work 8 / video

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,20 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
+import {Provider} from "react-redux";
+import {createBrowserRouter, RouterProvider} from "react-router";
 import App from "./App.tsx";
 
+const routes = createBrowserRouter([
+    {
+        path: "/",element:<App/>,children:[
+            {path: "users",element:null},
+            {path: "posts",element:null},
+            {path: "comments",element:null},
+            {path: "complex",element:null}
+        ]
+    }
+]);
 
-createRoot(document.getElementById('root')!).render(<App/>)
+createRoot(document.getElementById('root')!).render(<Provider store={store}>
+ <RouterProvider router={routes}/>
+</Provider>)
