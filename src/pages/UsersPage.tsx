62b8669dcba90e7cc2b30939a70747116b31b08d
Maietskyi <damaietskyi@gmail.com>
2025-01-30 00:16:50 +0200

    Add Lesson 8 / 3-redux

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -4,14 +4,15 @@ import {useAppDispatch} from "../redux/hooks/useAppDispatch.ts";
 import {userSliceActions} from "../redux/slices/userSlice/userSlice.ts";
 
 export const UsersPage = () => {
-    const {users} = useAppSelector(state => state.userSlice);
-const dispatch = useAppDispatch();
-useEffect(() => {
-    dispatch(userSliceActions.loadUsers())
-},[])
+    const {users, loadState} = useAppSelector(state => state.userSlice);
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(userSliceActions.loadUsers())
+    }, [])
 
-return (
+    return (
         <div>
+            {!loadState && <div>Loading</div>}
             {
                 users.map((user) => {
                     return <div key={user.id}>{user.name}</div>
