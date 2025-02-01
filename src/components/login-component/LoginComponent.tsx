360851260ea9fc69d175a4565390d1ad204c5569
Maietskyi <damaietskyi@gmail.com>
2025-02-01 20:58:53 +0200

    Update Project demo-6

---
+++ src/components/login-component/LoginComponent.tsx
@@ -0,0 +1,26 @@
+// import {useEffect} from "react";
+// import {login} from "../services/api.services.ts";
+//
+// // Компонент для автоматичного логіну користувача
+// const LoginComponent = () => {
+//     // хук useEffect - виконує дію логінації
+//     useEffect(() => {
+//         // Викликаємо функцію логіну при завантаженні компонента
+//         login({
+//             username: 'emilys',
+//             //  передаю параметр username - Логін з dummyjson
+//             password: 'emilyspass',
+//             //  передаю параметр password - Пароль з dummyjson
+//             expiresInMins: 1
+//             //  передаю параметр expiresInMins - Час сесії - 1 хвилина
+//         })
+//     }, []);
+// // Порожній масив залежностей, тому ефект виконується лише один раз
+//     return (
+//         <div>
+//
+//         </div>
+//     );
+// };
+//
+// export default LoginComponent;
